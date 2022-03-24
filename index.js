5914e2f744e7f72d2fb78f3f74c89182d04960ae
SUFFOSOHRAOUL <sohraoulsuffo@gmail.com>
2022-03-24 23:56:15 +0100

    node farm complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const http= require('http')
 const url= require('url')
-
+const replacetemplate = require('/home/suffo/Downloads/NEW/starter/module/replacetemplate')
 
 
 
@@ -27,20 +27,6 @@ fs.writeFileSync('/home/suffo/Downloads/NEW/starter/txt/output.txt' , textOut)
 //     })
     
 // })
-const replacetemplate = (temp,product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
-        output = output.replace(/{%IMAGE%}/g,product.image)
-         output = output.replace(/{%PRICE%}/g,product.price)
-         output = output.replace(/{%FROM%}/g,product.from)
-        output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
-        output = output.replace(/{%QUANTITY%}/g,product.quantity)
-        output = output.replace(/{%DESCRIPTION%}/g,product.description)
-        output = output.replace(/{%ID%}/g,product.id)
-
-
-      if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')      
-     return output
-    }
 
 
 
